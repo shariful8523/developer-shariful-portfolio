42f22940af3cd17fedcbe73e10e3bded3907833e
Md.Shariful Islam <mdsharifulislam8523@gmail.com>
2025-10-07 11:41:07 +0600

    added the Footer section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ import profileImage from '../../assets/my photo.jpg'
 const Home = () => {
     return (
         <section
-            id='home'
+            id='about'
             className='py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32'
         >
 
@@ -80,7 +80,7 @@ const Home = () => {
                     >
                         <img
                             src={profileImage}
-                            alt="Tarun Kaushik"
+                            alt="Shariful Islam"
                             className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
                         />
                     </Tilt>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   };
 
   const menuItems = [
-    { id: "home", label: "Home" },
+    // { id: "home", label: "Home" },
     { id: "about", label: "About" },
     { id: "skills", label: "Skills" },
     // { id: "experience", label: "Experience" },

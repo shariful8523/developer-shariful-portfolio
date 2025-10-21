ff0108641e7a2256597a70950e608723f08d76b0
Md.Shariful Islam <mdsharifulislam8523@gmail.com>
2025-10-22 02:46:09 +0600

    fixed project section

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -29,7 +29,7 @@ const Project = () => {
             </div>
 
             {/* Project Grid */}
-            <div className='grid gap-12  grid-cols-1 md:grid-cols-2 lg:grid-cols-3  '>
+            <div className='grid gap-12  grid-cols-1 md:grid-cols-2 lg:grid-cols-2  '>
                 {projects.map((project) => (
                     <div
                         key={project.id}
@@ -38,7 +38,7 @@ const Project = () => {
                                   hover:-translate-y-2 transition-transform duration-300'  >
 
                         <div className='p-4'>
-                            <img src={project.image} alt={project.title} className='w-full h-48 object-cover rounded-xl' />
+                            <img src={project.image} alt={project.title} className='w-full  object-cover rounded-xl ' />
                         </div>
                         <div className='p-6'>
                             <h3 className='text-2xl font-bold text-white mb-2'>{project.title}</h3>
@@ -62,61 +62,64 @@ const Project = () => {
 
             {/* Model Container  */}
 
-
             {selectedProject && (
-                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-90 p-4 ">
-                    <div className="bg-gray-900 rounded-xl shadow-2xl lg:w-full w-[90%] max-w-3xl overflow-hidden relative  ">
-                        <div className="flex justify-end p-4">
-                            <button
-                                onClick={handleCloseModal}
-                                className="text-white text-3xl font-bold hover:text-purple-500"
-                            >
-                                &times;
-                            </button>
-                        </div>
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-90 p-4">
+                    <div className="bg-gray-900 rounded-xl shadow-2xl lg:w-full w-[90%] max-w-3xl relative max-h-[90vh] flex flex-col">
+                        <button
+                            onClick={handleCloseModal}
+                            className="absolute top-2 right-3 z-20 text-white text-3xl font-bold hover:text-purple-500"
+                            aria-label="Close modal"
+                        >
+                            &times;
+                        </button>
+                        <div className="overflow-y-auto">
 
-                        <div className="flex flex-col">
-                            <div className="w-full flex justify-center bg-gray-900 px-4">
-                                <img
-                                    src={selectedProject.image}
-                                    alt={selectedProject.title}
-                                    className="lg:w-full w-[95%] object-contain rounded-xl shadow-2xl"
-                                />
-                            </div>
-                            <div className="lg:p-8 p-6">
-                                <h3 className="lg:text-3xl font-bold text-white mb-4 text-md">
-                                    {selectedProject.title}
-                                </h3>
-                                <p className="text-gray-400 mb-6 lg:text-base text-xs">
-                                    {selectedProject.description}
-                                </p>
-                                <div className="flex flex-wrap gap-2 mb-6">
-                                    {selectedProject.tags.map((tag, index) => (
-                                        <span
-                                            key={index}
-                                            className="bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1"
-                                        >
-                                            {tag}
-                                        </span>
-                                    ))}
+
+                            <div className="flex flex-col">
+
+
+                                <div className="w-full flex justify-center bg-gray-900 px-4 pt-12">
+                                    <img
+                                        src={selectedProject.image}
+                                        alt={selectedProject.title}
+                                        className="lg:w-full w-[95%] object-contain rounded-xl shadow-2xl"
+                                    />
                                 </div>
-                                <div className="flex gap-4">
-                                    <a
-                                        href={selectedProject.github}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="w-1/2 bg-gray-800 hover:bg-purple-800 text-gray-400 lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
-                                    >
-                                        View Code
-                                    </a>
-                                    <a
-                                        href={selectedProject.webapp}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
-                                    >
-                                        View Live
-                                    </a>
+                                <div className="lg:p-8 p-6">
+                                    <h3 className="lg:text-3xl font-bold text-white mb-4 text-md">
+                                        {selectedProject.title}
+                                    </h3>
+                                    <p className="text-gray-400 mb-6 lg:text-base text-xs">
+                                        {selectedProject.description}
+                                    </p>
+                                    <div className="flex flex-wrap gap-2 mb-6">
+                                        {selectedProject.tags.map((tag, index) => (
+                                            <span
+                                                key={index}
+                                                className="bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1"
+                                            >
+                                                {tag}
+                                            </span>
+                                        ))}
+                                    </div>
+                                    <div className="flex gap-4">
+                                        <a
+                                            href={selectedProject.github}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="w-1/2 bg-gray-800 hover:bg-purple-800 text-gray-400 lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
+                                        >
+                                            View Code
+                                        </a>
+                                        <a
+                                            href={selectedProject.webapp}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
+                                        >
+                                            View Live
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
